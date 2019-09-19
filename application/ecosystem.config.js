d05b0372bb3ddbabdf268155f8d879cbe930ea77
Johnson Wong <johnsonwong@Johnsons-MBP.hsd1.ca.comcast.net>
2019-09-18 23:30:08 -0700

    Setup PM2

---
+++ application/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'csc648-team03-sfsu-buy-sell',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-223-247-243.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/admin.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:CSC-648-SFSU/csc648-fall2019-Team03.git',
+      path: '/home/ubuntu/csc648-fall2019-Team03',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
