5681faff63da52f96319932cd3346edffdbc049e
Johnson Wong <johnsonwong@Johnsons-MBP.hsd1.ca.comcast.net>
2019-09-18 23:55:34 -0700

    fixed syntax

--- application/index.js
+++ application/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => {
-  res.send('HEY!')
-})
-app.listen(3000, () => console.log('Server running on port 3000'))
+  res.send('SFSU<br>CSC 648<br>Software Engineering<br>Fall 2019<br>Section 01<br>Team 03<br><br>Coming soon...');
+});
+app.listen(3000, () => console.log('Server running on port 3000'));
