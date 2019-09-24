4d6c5e70ffaf8f6ec3f7b264b7ffad84713a67f3
Johnson Wong <johnsonwong@Johnsons-MacBook-Pro.local>
2019-09-23 22:22:48 -0700

    refactored file/folder names, reorganized folder structure, code cleanup

--- application/routes/about.js
+++ application/routes/about.js
@@ -4,43 +4,43 @@ const express = require('express');
 const router = express.Router();
 
 // about home page
-router.get('/', function (req, res) {
-    res.render('about');
+router.get('/', function(req, res) {
+    res.render('about/about');
 });
 
 // steve's page
-router.get('/steve', function (req, res) {
-    res.render('steve');
+router.get('/steve', function(req, res) {
+    res.render('about/steve');
 });
 
 // johnson's page
-router.get('/johnson', function (req, res) {
-    res.render('johnson');
+router.get('/johnson', function(req, res) {
+    res.render('about/johnson');
 });
 
 // sergei's page
-router.get('/sergei', function (req, res) {
-    res.render('sergei');
+router.get('/sergei', function(req, res) {
+    res.render('about/sergei');
 });
 
 // tevis' page
-router.get('/tevis', function (req, res) {
-    res.render('tevis');
+router.get('/tevis', function(req, res) {
+    res.render('about/tevis');
 });
 
 // matthew's page
-router.get('/matthew', function (req, res) {
-    res.render('matthew');
+router.get('/matthew', function(req, res) {
+    res.render('about/matthew');
 });
 
 // tim's page
-router.get('/tim', function (req, res) {
-    res.render('tim');
+router.get('/tim', function(req, res) {
+    res.render('about/tim');
 });
 
 // osbaldo's page
-router.get('/osbaldo', function (req, res) {
-    res.render('osbaldo');
+router.get('/osbaldo', function(req, res) {
+    res.render('about/osbaldo');
 });
 
 module.exports = router;
