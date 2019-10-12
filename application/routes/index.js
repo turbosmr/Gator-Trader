67a9e6e3c2c222c039d064d265bc185c8631812a
STEVE RENTSCHLER <steverentschler@STEVEs-MacBook-Pro-2.local>
2019-10-11 17:27:12 -0700

    navbar and results page implemented
    navbar and results page implemented

--- application/routes/index.js
+++ application/routes/index.js
@@ -8,4 +8,12 @@ router.get('/', function (req, res) {
     res.render('home');
 });
 
+router.get('/account', function(req, res) {
+    res.render('account');
+});
+
+router.get('/results', function(req, res) {
+    res.render('results');
+});
+
 module.exports = router;
