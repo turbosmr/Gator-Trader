67e87cafe8d4b049531fa7fca00f40afc96fd19a
SergeiK <sergius16k@gmail.com>
2019-10-20 18:34:30 -0700

    Dashboard First Draft (Selling, Bought, Messages)

--- application/routes/account.js
+++ application/routes/account.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 // Render dashboard page
-router.get('/', (req, res) => {
+router.get('/dashboard', (req, res) => {
     res.render('dashboard');
 });
 
