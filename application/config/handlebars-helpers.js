d448ef402926ac681ffac5899391b06102a39f96
Johnson Wong <johnsonwong@Johnsons-MacBook-Pro.local>
2019-10-29 01:09:15 -0700

    finished pagination for results page

--- application/config/handlebars-helpers.js
+++ application/config/handlebars-helpers.js
@@ -1,5 +1,83 @@
 module.exports = {
-    isEqual: function(a, b) {
-        return a == b;
+    eq: (v1, v2) => {
+        return v1 == v2;
+    },
+    ne: (v1, v2) => {
+        return v1 != v2;
+    },
+    lt: (v1, v2) => {
+        return v1 < v2;
+    },
+    gt: (v1, v2) => {
+        return v1 > v2;
+    },
+    lte: (v1, v2) => {
+        return v1 <= v2;
+    },
+    gte: (v1, v2) => {
+        return v1 >= v2;
+    },
+    resultsInfo: (offset, pageLimit, totalProducts, keyword) => {
+        let html = "";
+        let posFromOffset = offset + pageLimit;
+
+        if (totalProducts > pageLimit) {
+            if (posFromOffset > totalProducts) {
+                posFromOffset = totalProducts;
+            }
+            html += "Showing " + (offset + 1) + "-" + posFromOffset + " of " + totalProducts;
+        }
+        else {
+            html += "Showing " + totalProducts;
+        }
+
+        if (totalProducts > 1) {
+            html += " results";
+        }
+        else {
+            html += " result";
+        }
+
+        if (keyword) {
+            html += " for <b>\"" + keyword + "\"</b>";
+        }
+
+        return html;
+    },
+    pagination: (pageCount, currentPage, category, keyword) => {
+        let html = "";
+        let x = currentPage;
+
+        if (!keyword) {
+            keyword = "";
+        }
+
+        // Previous button
+        if (x === 1) {
+            html += "<li class=\"page-item disabled\"><a class=\"page-link\" href=\"/search?c=" + category + "&k=" + keyword + "&page=" + (x - 1) + "\">Previous</a></li>";
+        }
+        else {
+            html += "<li class=\"page-item\"><a class=\"page-link\" href=\"/search?c=" + category + "&k=" + keyword + "&page=" + (x - 1) + "\">Previous</a></li>";
+        }
+
+        // Page button
+        for (let i = 1; i <= pageCount; i++) {
+            if (currentPage === i) {
+                html += "<li class=\"page-item active\"><a class=\"page-link\" href=\"/search?c=" + category + "&k=" + keyword + "&page=" + i + "\">" + i + "</a></li>";
+            }
+            else {
+                html += "<li class=\"page-item\"><a class=\"page-link\" href=\"/search?c=" + category + "&k=" + keyword + "&page=" + i + "\">" + i + "</a></li>";
+            }
+        }
+
+        // Next button
+        if (x >= pageCount) {
+            html += "<li class=\"page-item disabled\"><a class=\"page-link\" href=\"/search?c=" + category + "&k=" + keyword + "&page=" + (x - 1) + "\">Next</a></li>";
+        }
+        else {
+            html += "<li class=\"page-item\"><a class=\"page-link\" href=\"/search?c=" + category + "&k=" + keyword + "&page=" + (x + 1) + "\">Next</a></li>";
+        }
+
+        return html;
     }
-  }
+}
--- application/routes/search.js
+++ application/routes/search.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
+let pageLimit = 10;
+
 router.post('/', (req, res) => {
     let keyword = req.body.keyword;
     let category = req.body.categories;
@@ -21,7 +23,54 @@ router.post('/', (req, res) => {
     }
 });
 
-router.get('/', (req, res) => {
+let pages = (req, res, next) => {
+    let currentPage = parseInt(req.query.page) || 1;
+    let keyword = req.query.k;
+    let category = req.query.c;
+    let sql = "";
+    let placeholders = [];
+
+    if (keyword) {
+        if (category && category != 'all') {
+            sql = "SELECT COUNT(pid) AS total FROM SalesItem WHERE (name LIKE ? OR description LIKE ?) AND category = ?";
+            placeholders = ['%' + keyword + '%', '%' + keyword + '%', category];
+        }
+        else {
+            sql = "SELECT COUNT(pid) AS total FROM SalesItem WHERE name LIKE ? OR description LIKE ?";
+            placeholders = ['%' + keyword + '%', '%' + keyword + '%'];
+        }
+    }
+    else if (category && category != 'all') {
+        sql = "SELECT COUNT(pid) AS total FROM SalesItem WHERE category = ?";
+        placeholders = [category];
+    }
+    else {
+        sql = "SELECT COUNT(pid) AS total FROM SalesItem";
+    }
+
+    db.query(sql, placeholders, (error, result) => {
+        if (error) throw (error);
+
+        res.locals.currentPage = currentPage;
+        res.locals.totalPages = Math.ceil(parseInt(result[0].total) / pageLimit);
+        res.locals.totalProducts = result[0].total;
+
+        next();
+    });
+}
+
+router.get('/', pages, (req, res) => {
+    if (res.locals.currentPage > res.locals.totalPages || res.locals.currentPage < 1)
+        return res.redirect('/');
+
+    let limit = pageLimit;
+    let offset = (pageLimit * res.locals.currentPage) - pageLimit;
+
+    if (offset < 0) {
+        limit += offset;
+        offset = 0;
+    }
+
     let keyword = req.query.k;
     let category = req.query.c;
     let product = [];
@@ -30,20 +79,21 @@ router.get('/', (req, res) => {
 
     if (keyword) {
         if (category && category != 'all') {
-            sql = "SELECT * FROM SalesItem WHERE (name LIKE ? OR description LIKE ?) AND category = ?";
-            placeholders = ['%' + keyword + '%', '%' + keyword + '%', category];
+            sql = "SELECT * FROM SalesItem WHERE (name LIKE ? OR description LIKE ?) AND category = ? LIMIT ? OFFSET ?";
+            placeholders = ['%' + keyword + '%', '%' + keyword + '%', category, limit, offset];
         }
         else {
-            sql = "SELECT * FROM SalesItem WHERE name LIKE ? OR description LIKE ?";
-            placeholders = ['%' + keyword + '%', '%' + keyword + '%'];
+            sql = "SELECT * FROM SalesItem WHERE name LIKE ? OR description LIKE ? LIMIT ? OFFSET ?";
+            placeholders = ['%' + keyword + '%', '%' + keyword + '%', limit, offset];
         }
     }
     else if (category && category != 'all') {
-        sql = "SELECT * FROM SalesItem WHERE category = ?";
-        placeholders = [category];
+        sql = "SELECT * FROM SalesItem WHERE category = ? LIMIT ? OFFSET ?";
+        placeholders = [category, limit, offset];
     }
     else {
-        sql = "SELECT * FROM SalesItem";
+        sql = "SELECT * FROM SalesItem LIMIT ? OFFSET ?";
+        placeholders = [limit, offset];
     }
 
     db.query(sql, placeholders, (error, result) => {
@@ -52,9 +102,14 @@ router.get('/', (req, res) => {
             product.push(result[i]);
         }
         res.render('results', {
-            keyword: keyword,
             selectedCategory: category,
-            product: product
+            keyword: keyword,
+            product: product,
+            offset: offset,
+            pageLimit: limit,
+            totalProducts: res.locals.totalProducts,
+            pageCount: res.locals.totalPages,
+            currentPage: res.locals.currentPage
         });
     });
 });
@@ -79,7 +134,7 @@ router.get('/filter/price/:keyword', (req, res) => {
     let min;
     let max;
     let product = [];
-    
+
     if (price == "under25") {
         //handle logic to filter product results
         min = -0.01;
@@ -95,12 +150,12 @@ router.get('/filter/price/:keyword', (req, res) => {
         max = 10000000000.00;
     }
     let sql = "SELECT * FROM SalesItem WHERE (name LIKE ? OR description LIKE ?) AND (price > ?) AND (price < ?)";
-    db.query(sql, ['%' + keyword + '%','%' + keyword + '%', min, max], (error, result) => {
+    db.query(sql, ['%' + keyword + '%', '%' + keyword + '%', min, max], (error, result) => {
         if (error) throw error;
         for (let i = 0; i < result.length; i++) {
             product.push(result[i]);
         }
-        res.render('results', { 
+        res.render('results', {
             keyword: keyword,
             product: product,
             filter: price
