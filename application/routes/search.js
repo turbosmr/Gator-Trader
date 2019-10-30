9753a19b7a1c2f8beb8aef3b42293562b8fdb47a
Johnson Wong <johnsonwong@Johnsons-MBP.hsd1.ca.comcast.net>
2019-10-29 21:50:38 -0700

    refined logic when querying for results

--- application/routes/search.js
+++ application/routes/search.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
-let pageLimit = 10;
+const pageLimit = 10;
 
 router.post('/', (req, res) => {
     let keyword = req.body.keyword;
@@ -32,34 +32,41 @@ let pages = (req, res, next) => {
 
     if (keyword) {
         if (category && category != 'all') {
-            sql = "SELECT COUNT(pid) AS total FROM SalesItem WHERE (name LIKE ? OR description LIKE ?) AND category = ?";
+            sql = "SELECT * FROM SalesItem WHERE (name LIKE ? OR description LIKE ?) AND category = ?";
             placeholders = ['%' + keyword + '%', '%' + keyword + '%', category];
         }
         else {
-            sql = "SELECT COUNT(pid) AS total FROM SalesItem WHERE name LIKE ? OR description LIKE ?";
+            sql = "SELECT * FROM SalesItem WHERE name LIKE ? OR description LIKE ?";
             placeholders = ['%' + keyword + '%', '%' + keyword + '%'];
         }
     }
     else if (category && category != 'all') {
-        sql = "SELECT COUNT(pid) AS total FROM SalesItem WHERE category = ?";
+        sql = "SELECT * FROM SalesItem WHERE category = ?";
         placeholders = [category];
     }
     else {
-        sql = "SELECT COUNT(pid) AS total FROM SalesItem";
+        sql = "SELECT * FROM SalesItem";
     }
+
+    res.locals.sql = sql;
+    res.locals.placeholders = placeholders;
 
     db.query(sql, placeholders, (error, result) => {
         if (error) throw (error);
 
         res.locals.currentPage = currentPage;
-        res.locals.totalPages = Math.ceil(parseInt(result[0].total) / pageLimit);
-        res.locals.totalProducts = result[0].total;
+        res.locals.totalPages = Math.ceil(parseInt(result.length) / pageLimit);
+        res.locals.totalProducts = result.length;
 
         next();
     });
 }
 
 router.get('/', pages, (req, res) => {
+    let keyword = req.query.k;
+    let category = req.query.c;
+    let product = [];
+
     if (res.locals.currentPage > res.locals.totalPages || res.locals.currentPage < 1)
         return res.redirect('/');
 
@@ -70,37 +77,17 @@ router.get('/', pages, (req, res) => {
         limit += offset;
         offset = 0;
     }
+    
+    res.locals.sql += " LIMIT ? OFFSET ?";
+    res.locals.placeholders.push(limit, offset);
 
-    let keyword = req.query.k;
-    let category = req.query.c;
-    let product = [];
-    let sql = "";
-    let placeholders = [];
-
-    if (keyword) {
-        if (category && category != 'all') {
-            sql = "SELECT * FROM SalesItem WHERE (name LIKE ? OR description LIKE ?) AND category = ? LIMIT ? OFFSET ?";
-            placeholders = ['%' + keyword + '%', '%' + keyword + '%', category, limit, offset];
-        }
-        else {
-            sql = "SELECT * FROM SalesItem WHERE name LIKE ? OR description LIKE ? LIMIT ? OFFSET ?";
-            placeholders = ['%' + keyword + '%', '%' + keyword + '%', limit, offset];
-        }
-    }
-    else if (category && category != 'all') {
-        sql = "SELECT * FROM SalesItem WHERE category = ? LIMIT ? OFFSET ?";
-        placeholders = [category, limit, offset];
-    }
-    else {
-        sql = "SELECT * FROM SalesItem LIMIT ? OFFSET ?";
-        placeholders = [limit, offset];
-    }
-
-    db.query(sql, placeholders, (error, result) => {
+    db.query(res.locals.sql, res.locals.placeholders, (error, result) => {
         if (error) throw error;
+
         for (let i = 0; i < result.length; i++) {
             product.push(result[i]);
         }
+
         res.render('results', {
             selectedCategory: category,
             keyword: keyword,
@@ -121,9 +108,11 @@ router.get('/suggestions/typeahead', (req, res) => {
 
     db.query(sql, ['%' + keyword + '%', '%' + keyword + '%'], (error, result) => {
         if (error) throw error;
+
         for (let i = 0; i < result.length; i++) {
             product.push(result[i].name);
         }
+
         res.send(JSON.stringify(product));
     });
 });
