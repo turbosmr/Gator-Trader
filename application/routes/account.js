a8427d71fe83c54bdfdee059c41fe92bb14e61f5
SergeiK <sergius16k@gmail.com>
2019-11-03 14:57:20 -0800

    admin dashboard added + styling

--- application/routes/account.js
+++ application/routes/account.js
@@ -11,20 +11,25 @@ router.get('/dashboard', (req, res) => {
         let product = [];
         let sql = "SELECT SalesItem.name, SalesItem.status, Category.name AS category, SalesItem.price FROM SalesItem INNER JOIN Category ON SalesItem.category = Category.cid WHERE seller = ? AND status != 'unapproved'";
         let placeholders = [req.user.sid];
-        
+
         db.query(sql, placeholders, (error, result) => {
             if (error) throw error;
 
             for (let i = 0; i < result.length; i++) {
-                 product.push(result[i]);
+                product.push(result[i]);
             }
-            
+
             res.render('dashboard', { product: product });
         });
     }
     else {
         res.render('dashboard');
     }
+});
+
+//Admin dashboard Route
+router.get('/adminDashboard', (req, res) => {
+    res.render('adminDashboard');
 });
 
 module.exports = router;
