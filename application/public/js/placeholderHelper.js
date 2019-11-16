096dc309cf2ae5c05cfba3f7ef3417202d76a0e7
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-11-15 22:17:49 -0800

    added a placeholder image for class materials category.

--- application/public/js/placeholderHelper.js
+++ application/public/js/placeholderHelper.js
@@ -1,3 +1,5 @@
+//this function makes it possible to change the value of the placeholder text in the searchbar
+//Author @Osbaldo Martinez
 function changePlaceholder() {
     if(document.getElementById('dropDownCategories').value == 9) {
         document.getElementById('searchKeyword').setAttribute('placeholder', 'Please enter your class section (ex: CSC 648)');
